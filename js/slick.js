f674d72bf51ae4f39b958dc156e3b406c94e36c6
Einar2 <83465116+Einar2@users.noreply.github.com>
2021-05-15 19:59:05 +0300

    a

--- js/slick.js
+++ js/slick.js
@@ -35,6 +35,11 @@ function push(){
     d.style.opacity = ".1";
     e.style.opacity = ".1";
 
+    for(let i = 0; i < img.length; i++){
+    img[i].onclick = zoom;
+    clearInterval(time);
+}
+
 
 
 
@@ -42,6 +47,23 @@ function push(){
 
 
 
+function zoom(){
+    for (let j = 0; j < img.length; j++){
+        img[j].style.height = "600px";
+        img[j].onclick = not_zoom;
+    }
+
+}
+
+function not_zoom(){
+    for (let j = 0; j < img.length; j++){
+        img[j].style.height = "400px";
+        img[j].onclick = zoom;
+        img[j].onclick = push;
+    }
+
+
+}
 
 
 
@@ -60,8 +82,114 @@ function close(){
     for (let j = 0; j < img.length; j++){
         img[j].classList.remove("img_block_img");
     }
-
+    not_zoom();
     d.style.opacity = "1";
     footer.style.opacity = "1";
     e.style.opacity = "1";
 }
+
+
+//
+
+
+let images = document.querySelectorAll(".img_slider");
+let blocks = document.querySelectorAll(".img_block");
+let circles = document.querySelectorAll(".slide_circle");
+let step = 0;
+
+
+function slider(){
+
+    for(let i = 0; i < images.length; i++){
+        images[i].classList.add("opacity0");
+
+
+    }
+
+    images[step].classList.remove("opacity0");
+
+    setTimeout(function(){
+        slider_2();
+    },1000)
+
+ //circle
+    for(let k = 0; k < circles.length; k++ ){
+        circles[k].classList.remove("active_slide");
+    }
+
+    circles[step].classList.add("active_slide");
+
+
+}
+
+
+
+function slider_2(){
+    for(let j = 0; j < blocks.length; j++){
+        blocks[j].classList.add ("delay");
+    }
+        blocks[step].classList.remove("delay");
+}
+
+slider_2();
+slider();
+
+
+ let time = setInterval(function(){
+if(step + 1 == images.length){
+        step = 0;
+    }
+    else{
+        step++;
+    }
+    slider();
+
+},5000);
+
+
+
+//BTN
+
+
+document.querySelector(".left").onclick = function(){
+
+    if(step - 1 == -1){
+        step = images.length - 1;
+    }
+    else{
+        step--;
+    }
+    slider();
+    clearInterval(time);
+
+
+    //
+
+
+    //
+};
+
+
+
+document.querySelector(".rigth").onclick = function(){
+    if(step + 1 == images.length){
+        step = 0;
+    }
+    else{
+        step++;
+    }
+    slider();
+    clearInterval(time);
+
+
+    //
+
+
+
+
+
+
+
+    //
+
+};
