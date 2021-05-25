f66fac615c28ed0300b523a45e4262b7c64b5e2e
Einar2 <83465116+Einar2@users.noreply.github.com>
2021-05-25 17:27:21 +0300

    s

--- js/gor.js
+++ js/gor.js
@@ -1,5 +1,1 @@
-var screenOrientation  = orientationscreen.orientation || screen.mozOrientation || screen.msOrientation;
 
-if (screenOrientation  === "portrait-primary" || Math.abs(window.orientation) !== 90) {
-  document.querySelector("body").style.display = "none";
-}
