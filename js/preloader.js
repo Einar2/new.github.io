79b1d4b6d42654f62cb66e8eff188a849ca44731
Einar2 <83465116+Einar2@users.noreply.github.com>
2021-05-25 21:56:10 +0300

    n

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,13 @@
 window.onload = function (){
     document.querySelector(".preloader").style.display = "none";
+
+    let logo = document.querySelectorAll(".logo_gif");
+    for(let i = 0; i < logo.length; i++ ){
+        logo[i].classList.add("b2");
+    }
+    document.querySelector(".footer").classList.add("b3");
+
     document.querySelector(".img_title").classList.add("b1");
-    document.querySelector(".logo_gif").classList.add("b2");
 
 
 
